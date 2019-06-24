76a479b25130fc113471f5b18b5ea31a7f8609dd
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-06-24 14:17:23 +0700

    pre define default state

--- src/App.js
+++ src/App.js
@@ -11,35 +11,44 @@ class Cell extends React.Component {
         onClick={(e) => this.setState({
           number: (this.state.number + 1) % 5
         })}
-        className="cell"
+        className={`cell ${this.props.isInitial ? 'initial': ''}`}
       >
         {this.state.number !== 0 && this.state.number}
       </div>
     );
   }
 }
+class Board extends React.Component { 
+  state = {
+    board: [
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+      [1, 2, 3, 4],
+      [1, 2, 3, 4]
+    ],
+    initial: [
+      [true, true, true, false],
+      [true, true, true, false],
+      [true, false, true, true],
+      [true, true, true, false]
+    ],
+  }
+  render() { 
+    return (
+      <div className="board">
+        {
+          this.state.board.map((row, i) => row.map((number, j) => <Cell key={`cell-${i}-${j}`} number={number} initial={this.state.initial[i][j]} />))
+        }
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="board">
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-        </div>
+        <Board />
       </div>
     );
   }
