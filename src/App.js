626fad8d35a9fdd1d30c130bc1b325967d808e6a
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-06-24 14:49:52 +0700

    Validation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
+const validate = board => {
+  let isValid = true;
+  for (let i = 0; i < 4; i++) {
+    const horizontal = new Set();
+    const vertical = new Set();
+    const square = new Set();
+    for (let j = 0; j < 4; j++) {
+      horizontal.add(board[i][j]);
+      vertical.add(board[j][i]);
+      square.add(
+        board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)]
+      );
+    }
+    horizontal.delete(0);
+    vertical.delete(0);
+    square.delete(0);
+    if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+      isValid = false;
+    }
+  }
+  return isValid;
+};
 class Cell extends React.Component { 
-
   render() {
     return (
       <div
-        onClick={(e) =>
-          this.setState({
-            number: (this.props.number + 1) % 5
-          })
+        onClick={(e) => {
+            if (this.props.isInitial) {
+              return
+            }
+            this.props.onChange((this.props.number+1)%5)
+          }
         }
         className={`cell ${this.props.isInitial ? 'initial': ''}`}
       >
         {this.props.number !== 0 && this.props.number}
       </div>
-    );
+    )
   }
 }
 class Board extends React.Component { 
@@ -32,24 +55,38 @@ class Board extends React.Component {
       [true, false, true, false],
       [true, false, false, true],
     ],
+    statusText: 'test'
+  }
+  submit = () => {
+    const isValid = validate(this.state.board)
+    this.setState({ statusText: isValid ? 'Board is Complete!' : 'Board is invalid!' })
   }
   render() { 
     return (
-      <div className="board">
-        {
-          this.state.board.map(
-            (row, i) => row.map(
-              (number, j) =>
-                <Cell
-                  key={`cell-${i}-${j}`}
-                  number={number}
-                  isInitial={this.state.initial[i][j]}
-                />
+      <div>
+        <div className="board">
+          {
+            this.state.board.map(
+              (row, i) => row.map(
+                (number, j) =>
+                  <Cell
+                    key={`cell-${i}-${j}`}
+                    number={number}
+                    isInitial={this.state.initial[i][j]}
+                    onChange={(newNumber) => {
+                      const { board } = this.state
+                      board[i][j] = newNumber
+                      this.setState({board})
+                    }}
+                  />
+              )
             )
-          )
-        }
+          }
+        </div>
+        <button onClick={this.submit}>Submit</button>
+        <p>{this.state.statusText}</p>
       </div>
-    );
+    )
   }
 }
 
@@ -59,8 +96,8 @@ class App extends React.Component {
       <div className="App">
         <Board />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
