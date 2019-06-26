66f80992255a9af825fe6adbe672bd209517291a
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-06-26 10:32:08 +0700

    Updates

---
+++ precache-manifest.40a2e7a5a468e5725e3620de607a712b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa5b92139a5bcd26c7c6ee4f9f7b920",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "611c57a4124bfb269b25",
+    "url": "/react-sudoku/static/css/main.b1d84762.chunk.css"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/react-sudoku/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "611c57a4124bfb269b25",
+    "url": "/react-sudoku/static/js/main.ab79f477.chunk.js"
+  },
+  {
+    "revision": "6a7250d3c025ff65bfba",
+    "url": "/react-sudoku/static/js/runtime~main.6891cdef.js"
+  }
+]);
